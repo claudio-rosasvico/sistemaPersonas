b1723fe28ec3e9e4fe8f7ed01c717477b3036d47
claudio-rosasvico <claudio.rosasvico@gmail.com>
2024-02-03 13:47:24 -0300

    logo icon

--- resources/views/afiliados/index.blade.php
+++ resources/views/afiliados/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/dedos_V.png">
     <link href="{{ asset('black') }}/css/black-dashboard.css" rel="stylesheet" />
     <link href="{{ asset('black') }}/css/theme.css" rel="stylesheet" />
     <link rel="stylesheet"
